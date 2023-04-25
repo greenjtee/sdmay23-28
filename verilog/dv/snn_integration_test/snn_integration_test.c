2acb3220e8e4f241c6e3b819b4e7f13c119f5b29
tjgreen <greenjtee@gmail.com>
2023-04-24 21:52:45 -0500

    better test

--- verilog/dv/snn_integration_test/snn_integration_test.c
+++ verilog/dv/snn_integration_test/snn_integration_test.c
@@ -19,68 +19,86 @@
 #include <defs.h>
 #include <stub.c>
 
-#define reg_snn_control (*(volatile uint32_t*)0x30004000)
+#define reg_snn_control (*(volatile uint32_t *)0x30004000)
 
-#define image_write(data, index) (*(volatile uint32_t*)(0x30000000 + (index << 2))) = data
-#define weight0_write(data, index) (*(volatile uint32_t*)(0x30001000 + (index << 2))) = data
-#define weight1_write(data, index) (*(volatile uint32_t*)(0x30002000 + (index << 2))) = data
+#define image_write(data, index) (*(volatile uint32_t *)(0x30000000 + (index << 2))) = data
+#define weight0_write(data, index) (*(volatile uint32_t *)(0x30001000 + (index << 2))) = data
+#define weight1_write(data, index) (*(volatile uint32_t *)(0x30002000 + (index << 2))) = data
 
 #define WIDTH 14
 #define HEIGHT 14
 #define OUTPUTS 10
-#define NUM_PIXELS HEIGHT*WIDTH
-#define NUM_WEIGHTS NUM_PIXELS*OUTPUTS
+#define NUM_PIXELS HEIGHT *WIDTH
+#define NUM_WEIGHTS NUM_PIXELS *OUTPUTS
 
-// uint8_t image[NUM_PIXELS] = {0};
+uint8_t image[NUM_PIXELS];
+
+// uint8_t image[NUM_PIXELS] = {
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// };
 
 void main()
 {
 	int j;
 
-	// uint8_t weights[NUM_WEIGHTS];
+	uint8_t weights[NUM_WEIGHTS];
 
-        reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
-        reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
 
-        reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_5  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_4  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_3  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_2  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_1  = GPIO_MODE_USER_STD_OUTPUT;
-        reg_mprj_io_0  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_9 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_8 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_7 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_5 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_4 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_3 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_2 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_1 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_0 = GPIO_MODE_USER_STD_OUTPUT;
 
-        reg_mprj_io_6  = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_6 = GPIO_MODE_MGMT_STD_OUTPUT;
 
 	reg_wb_enable = 1;
 
-    // Now, apply the configuration
-    reg_mprj_xfer = 1;
-    while (reg_mprj_xfer == 1);
+	// Now, apply the configuration
+	reg_mprj_xfer = 1;
+	while (reg_mprj_xfer == 1)
+		;
 
-	// Flag start of the test 
+	// Flag start of the test
 	reg_mprj_datal = 0xAB400000;
 
 	// initialize user area
@@ -88,15 +106,24 @@ void main()
 
 	uint32_t i;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < NUM_PIXELS; i++)
 	{
-		image_write(i, i);
+		image_write(image[i], i);
+	}
+
+	for (i = 0; i < NUM_WEIGHTS / 2; i++)
+	{
+		weight0_write(weights[i], i);
+	}
+
+	for (i = 0; i < NUM_WEIGHTS / 2; i++)
+	{
+		weight1_write(weights[i + NUM_WEIGHTS / 2], i);
 	}
 
 	reg_snn_control |= (0x1 << 26);
 
 	// done initializing user area
-
 
 	reg_mprj_datal = 0xAB410000;
 	reg_mprj_datal = 0xAB510000;
